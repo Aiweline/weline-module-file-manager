705372614f21d0cff171231418de7bd2e4f8766a
秋枫雁飞 <1714255949@qq.com>
2023-10-03 20:19:15 +0800

    依赖添加

--- register.php
+++ register.php
@@ -14,5 +14,8 @@ Register::register(
     'Weline_FileManager',
     __DIR__,
     '1.0.0',
-    'ElFinder文件管理器.你可以修改前后端的elfinder.html模板文件，修改成你想要的样式。一旦前后端存在此elfinder.html模板文件，更新或者安装不会受到影响。'
+    'ElFinder文件管理器.你可以修改前后端的elfinder.html模板文件，修改成你想要的样式。一旦前后端存在此elfinder.html模板文件，更新或者安装不会受到影响。',
+    [
+        'Weline_MediaManager'
+    ]
 );
